6422a96551c96f44f8ee0b4e687bdab29ba03bb9
Tom Solberg <me@sbg.dev>
2024-10-22 10:33:51 +0200

    include cu117

--- cmd/scrape/main.go
+++ cmd/scrape/main.go
@@ -14,6 +14,7 @@ var URLs = []string{
 	"https://download.pytorch.org/whl/nightly/",
 	"https://download.pytorch.org/whl/cu121/",
 	"https://download.pytorch.org/whl/cu118/",
+	"https://download.pytorch.org/whl/cu117/",
 	"https://download.pytorch.org/whl/cu116/",
 	"https://download.pytorch.org/whl/cu110/",
 	"https://download.pytorch.org/whl/cu111/",
